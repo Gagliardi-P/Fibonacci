d3d68e5acb488871a7aaba2253cf0c693b0f71eb
Pietro Gagliardi <113630799+Gagliardi-P@users.noreply.github.com>
2022-09-22 02:22:01 +0000

    Recursiva funcional

--- main.c
+++ main.c
@@ -3,6 +3,14 @@
 
 #include <stdio.h>
 
+unsigned int fact(unsigned int n) {
+  if (n==0 || n == 1)
+    return 1;
+  return n * fact(n-1);
+  }
+
+
+
 int main(){
   int n , i, acc = 1; // Declaro as minhas variáveis
   scanf("%d", &n); //Leio o número de fibonacci digitado
@@ -10,6 +18,7 @@ int main(){
     acc*=i;//acc*i
   }
   printf("n!=%d\n", acc);    //Retorno o valor dos meus cálculos
+  printf("n!=%d", acc, fact(n));
   
   return 0;
 }
